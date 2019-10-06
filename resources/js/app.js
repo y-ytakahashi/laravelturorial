d5da7ceb7e4893eb17ce0c3a99052c1a5a2bd041
yuta takahashi <y.takahashi@inter-zone.jp>
2019-10-07 00:56:35 +0900

    6章 vuex 登録後のトップページへの移動

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 // ルーティングの定義をインポートする
 import router from './router'
+
 // ルートコンポーネントをインポートする
 import App from './App.vue'
+
+// 認証情報を保持する(vuex)
+import store from './store' // ★　追加
+
+import './bootstrap' // CSRF 対策組み込み済 
+
 
 new Vue({
   el: '#app',
   router, // ルーティングの定義を読み込む
+  store, // 認証情報の保持(vuex)
   components: { App }, // ルートコンポーネントの使用を宣言する
   template: '<App />' // ルートコンポーネントを描画する
 })
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,41 @@
 <?php
+
+
 // 会員登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
 // ログイン
 Route::post('/login', 'Auth\LoginController@login')->name('login');
+
 // ログアウト
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+// ログインユーザー
+Route::get('/user', function () {
+    return Auth::user();
+})->name('user');
+
+// トークンリフレッシュ
+Route::get('/reflesh-token', function (Illuminate\Http\Request $request) {
+    $request->session()->regenerateToken();
+
+    return response()->json();
+});
+
+// 写真投稿
+Route::post('/photos', 'PhotoController@create')->name('photo.create');
+
+// 写真一覧
+Route::get('/photos', 'PhotoController@index')->name('photo.index');
+
+// 写真詳細
+Route::get('/photos/{id}', 'PhotoController@show')->name('photo.show');
+
+// コメント
+Route::post('/photos/{photo}/comments', 'PhotoController@addComment')->name('photo.comment');
+
+// いいね
+Route::put('/photos/{id}/like', 'PhotoController@like')->name('photo.like');
+
+// いいね解除
+Route::delete('/photos/{id}/like', 'PhotoController@unlike');
